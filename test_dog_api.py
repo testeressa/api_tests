c490478969aedc09522045db25f43d1c4ee3506e
Maria Kosheleva <testeressa>
2025-05-30 11:01:03 +0300

    внесла правки после code review

--- test_dog_api.py
+++ test_dog_api.py
@@ -19,17 +19,40 @@ def test_get_random_breed_image():
 def test_get_random_image_by_breed(breed):
     resp = requests.get(f'{BASE_URL}/breed/{breed}/images/random')
     assert resp.status_code == 200
-    assert resp.json()["status"] == "success"
+
+    data = resp.json()
+    assert data["status"] == "success"
+
+    image_url = data["message"]
+    assert breed in image_url.lower(), f"Ожидалось изображение породы '{breed}', но получен урл: {image_url}"
 
 
 @pytest.mark.parametrize('breed,subbreed', [('hound', 'afghan'), ('bulldog', 'french')])
 def test_get_sub_breed_images(breed, subbreed):
     resp = requests.get(f'{BASE_URL}/breed/{breed}/{subbreed}/images')
     assert resp.status_code == 200
-    assert isinstance(resp.json()['message'], list)
+
+    data = resp.json()
+    assert isinstance(data['message'], list), "Ответ должен содержать список изображений"
+    assert len(data['message']) > 0, "Список изображений не должен быть пустым"
+
+    for image_url in data['message']:
+        assert breed in image_url.lower() and subbreed in image_url.lower(), \
+            f"Ожидались изображения {breed}/{subbreed}, но получен URL: {image_url}"
 
 
-def test_multiple_images():
-    resp = requests.get(f'{BASE_URL}/breed/hound/afghan/images/random/3')
+@pytest.mark.parametrize('subbreed,count', [('afghan', 1), ('blood', 5),])
+def test_get_multiple_images_by_subbreed(subbreed, count):
+    breed = 'hound'
+    resp = requests.get(f'{BASE_URL}/breed/{breed}/{subbreed}/images/random/{count}')
+
     assert resp.status_code == 200
-    assert len(resp.json()['message']) == 3
+
+    data = resp.json()
+
+    assert isinstance(data['message'], list)
+    assert len(data['message']) == count, f"Ожидалось {count} изображений, получено {len(data['message'])}"
+
+    for image_url in data['message']:
+        assert breed in image_url.lower() and subbreed in image_url.lower(), \
+            f"Ожидались изображения {breed}/{subbreed}, но получен URL: {image_url}"
--- test_openbrewerydb.py
+++ test_openbrewerydb.py
@@ -4,34 +4,73 @@ import requests
 BASE_URL = "https://api.openbrewerydb.org/v1"
 
 
-def test_get_breweries_per_page():
-    per_page = 3
-    resp = requests.get(f'{BASE_URL}/breweries?per_page={per_page}')
-    assert resp.status_code == 200
-    assert len(resp.json()) == per_page
+@pytest.mark.parametrize('per_page', [0, -1, 3])
+def test_get_breweries_per_page(per_page):
+    try:
+        resp = requests.get(f'{BASE_URL}/breweries?per_page={per_page}')
+        resp.raise_for_status()
+
+        data = resp.json()
+
+        assert resp.status_code == 200
+
+        if per_page > 0:
+            assert isinstance(data, list)
+            assert len(data) == per_page
+        else:
+            assert isinstance(data, dict)
+            assert "message" in data
+
+    except requests.exceptions.RequestException as e:
+        pytest.fail(f"Ошибка при запросе: {e}")
 
 
 @pytest.mark.parametrize('city', ['Marseille', 'Louisville', 'San Diego'])
 def test_get_breweries_by_city(city):
     resp = requests.get(f'{BASE_URL}/breweries?by_city={city}')
+    resp.raise_for_status()
+
+    data = resp.json()
+
     assert resp.status_code == 200
     assert isinstance(resp.json(), list)
+    assert len(data) > 0, f"Для города {city} получен пустой список пивоварен"
+
+    for brewery in data:
+        assert city.lower() in brewery["city"].lower(), f"Ожидался город {city}, получен {brewery['city']} "\
+                                                        f"(полные данные: {brewery})"
 
 
-@pytest.mark.parametrize("name", ["Almanac Beer Company", "Russian River Brewing Company"])
+@pytest.mark.parametrize("name", ["Almanac Beer Company", "Gordon Biersch Brewery Restaurant - San Diego"])
 def test_search_breweries_by_name(name):
-    response = requests.get(f"{BASE_URL}/breweries?by_name={name}")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
+    resp = requests.get(f"{BASE_URL}/breweries?by_name={name}")
+    assert resp.status_code == 200
+
+    data = resp.json()
+    assert isinstance(data, list)
+    assert len(data) > 0, f"Не найдено пивоварен по имени '{name}'"
+
+    for brewery in data:
+        assert name.lower() in brewery["name"].lower(), \
+            f"Ожидалось имя содержащее '{name}', получено '{brewery['name']}'"
 
 
 def test_list_breweries():
-    response = requests.get(f"{BASE_URL}/breweries")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
+    resp = requests.get(f"{BASE_URL}/breweries")
+
+    data = resp.json()
+
+    assert resp.status_code == 200
+    assert isinstance(data, list)
+    assert len(data) > 0, "Сервер вернул пустой список пивоварен"
 
 
 def test_get_random_brewery():
-    response = requests.get(f"{BASE_URL}/breweries/random")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
+    resp = requests.get(f"{BASE_URL}/breweries/random")
+    assert resp.status_code == 200
+
+    data = resp.json()
+
+    assert isinstance(data, list)
+    assert len(data) > 0, "Сервер вернул пустой список случайных пивоварен"
+    assert len(data) == 1, f"Ожидалась одна пивоварня, получено {len(data)}"
